20d932eaefab68e6d8410d06def25381f3efc4fd
aroshmp <arosh.r583@gmail.com>
2025-11-03 19:03:20 +1300

    Add Helloworld component and update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Helloworld from "./components/Helloworld";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World! Welcome to my React application.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Helloworld/>
     </div>
   );
 }
